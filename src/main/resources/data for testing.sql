1943ae568302b814b859d6f2783e7a5580a690bb
Julian <julianperazo93@gmail.com>
2023-09-10 21:29:22 -0500

    third commit

---
+++ src/main/resources/data for testing.sql
@@ -0,0 +1,11 @@
+-- Insert product
+INSERT INTO product (name, reference, price, weight, category, stock)
+VALUES ('Product 1', 'REF-001', 10.00, 1.0, 'Electronic', 10);
+
+-- Insert product
+INSERT INTO product (name, reference, price, weight, category, stock)
+VALUES ('Product 2', 'REF-002', 20.00, 2.0, 'Food', 20);
+
+-- Insert product
+INSERT INTO product (name, reference, price, weight, category, stock)
+VALUES ('Product 3', 'REF-003', 30.0, 3.0, 'Transport', 30);
